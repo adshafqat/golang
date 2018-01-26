d751bc39710866eb5b63bbe4270975e6376eb913
Root User <root@localhost>
2018-01-26 22:18:45 +0000

    Another update

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -2,26 +2,19 @@ package main
 
 import (
     "net/http"
-    "log"
     "fmt"
-    "httprouter"
-)
+    )
 
 
 
 func main() {
-    router := httprouter.New()
-    router.GET("/", Index)
-    router.GET("/hello/:name", Hello)
 
-    log.Fatal(http.ListenAndServe(":8080", router))
+   http.HandleFunc("/",handler)
+   http.ListenAndServe(":8080",nil)
 
  }
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-    fmt.Fprint(w, "Welcome!\n")
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, this is my first GoLang program! %s",r.URL.Path[1:])
 }
 
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-    fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
-}
 
