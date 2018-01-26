6bf76461a8841f158f18062f7a57a76b0cd9eddd
Root User <root@localhost>
2018-01-26 13:59:28 +0000

    First commit

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+    "net/http"
+    "log"
+    "fmt"
+    "httprouter"
+)
+
+
 
 func main() {
-    fmt.Printf("Hi, My name is Adeel Shafqat\n")
+    router := httprouter.New()
+    router.GET("/", Index)
+    router.GET("/hello/:name", Hello)
+
+    log.Fatal(http.ListenAndServe(":8080", router))
+
+ }
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+    fmt.Fprint(w, "Welcome!\n")
 }
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
+
